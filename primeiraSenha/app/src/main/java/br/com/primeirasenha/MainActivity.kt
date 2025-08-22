ef1ffbbde7339a66451bbbf6f02993f0f3ac4d8a
GABRIEL SIMON <gabriel_simon1@estudante.sc.senai.br>
2025-08-22 18:58:42 -0300

    Teste

--- primeiraSenha/app/src/main/java/br/com/primeirasenha/MainActivity.kt
+++ primeiraSenha/app/src/main/java/br/com/primeirasenha/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val mensagem = findViewById<TextView>(R.id.mensagem);
         mensagem.text = "";
 
